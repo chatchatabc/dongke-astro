181e86030422ba44d096c3914487274579e4cd94
BoJoNVi <bonjomontes@gmail.com>
2023-09-01 11:36:27 +0800

    build: integrated astro-imagetools

---
+++ astro-imagetools.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro-imagetools/config";
+
+export default defineConfig({
+  format: ["webp"],
+  fallbackFormat: "webp",
+  includeSourceFormat: false,
+  layout: "fill",
+  fadeInTransition: false,
+});
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
 });
