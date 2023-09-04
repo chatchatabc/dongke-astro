9cb489f9aaef0f0e41bdbf06fd66e3ceab328e04
BoJoNVi <bonjomontes@gmail.com>
2023-09-04 10:45:37 +0800

    fix: can't navigate in zh locale

--- src/services/utilService.ts
+++ src/services/utilService.ts
@@ -1,3 +1,5 @@
+import i18next from "i18next";
+
 export function utilFixPathname(pathname: string): string {
   return pathname[pathname.length - 1] === "/"
     ? pathname.slice(0, -1)
@@ -9,4 +11,14 @@ export function utilSlugToTitle(slug: string): string {
     .split("-")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
+}
+
+export function utilAddLocalePathname(params: { pathname: string }) {
+  const language = i18next.language;
+  const { pathname } = params;
+
+  if (language === "en") {
+    return pathname;
+  }
+  return `/${language}${pathname}`;
 }
