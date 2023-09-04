3e06844d63f96f759809ce16ffb6840e4c1d7d52
BoJoNVi <bonjomontes@gmail.com>
2023-09-04 14:00:59 +0800

    chore: created some content data for the news

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const newsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    timestamp: z.number(),
+    summary: z.string().optional(),
+    image: z.string(),
+  }),
+});
+
+export const collections = {
+  news: newsCollection,
+};
