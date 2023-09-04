e8b10496458864a131148bd01e9129caeefe54db
BoJoNVi <bonjomontes@gmail.com>
2023-09-04 13:52:41 +0800

    build: support for mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import astroI18next from "astro-i18next";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://dongke.pages.dev",
   server: {
     port: 3000,
   },
-  integrations: [tailwind(), astroI18next(), astroImageTools],
+  integrations: [tailwind(), astroI18next(), astroImageTools, mdx()],
 });
