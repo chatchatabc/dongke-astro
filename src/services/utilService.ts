c0f86e9bc966b24d923e232125bc26d5583bee8f
BoJoNVi <bonjomontes@gmail.com>
2023-09-04 10:59:31 +0800

    fix: inner layout nav translation

--- src/services/utilService.ts
+++ src/services/utilService.ts
@@ -22,3 +22,27 @@ export function utilAddLocalePathname(params: { pathname: string }) {
   }
   return `/${language}${pathname}`;
 }
+
+export function utilRemoveLocalePathname(params: { pathname: string }) {
+  const language = i18next.language;
+  const { pathname } = params;
+
+  if (language === "en") {
+    return pathname;
+  }
+  return pathname.replace(`/${language}`, "");
+}
+
+export function utilConvertSlugToCamelCase(params: { slug: string }): string {
+  const { slug } = params;
+  const words = slug.split("-");
+  const camelCase = words
+    .map((word, index) => {
+      if (index === 0) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join("");
+  return camelCase;
+}
